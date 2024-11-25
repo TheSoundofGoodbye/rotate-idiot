678d42592a0064b2b007c46511b2746d7490623e
TheSoundOfGoodBye <karbehemoth@gmail.com>
2024-11-25 23:25:04 +0900

    rendering single side

--- app/page.tsx
+++ app/page.tsx
@@ -1,87 +1,102 @@
 import Image from "next/image";
 
-//Canvas is twice the size of the square of cubes
-function generateCanvas(size: number): number[][] {
-  const canvas = Array(size * 2).fill(null).map(() => Array(size * 2).fill(0));
-  return canvas;
-}
 
 //Generate a 3D matrix of size x size x size and put each side of the cube in the matrix
-function generate3DMatrix(size: number): any {
+function calculateCube(size: number): any {
+  //Canvas is twice the size of the square of cubes and has 6 layers
+  let canvas = Array(size*2).fill(null).map(() => 
+    Array(size*2).fill(null).map(() => 
+     Array(6).fill(null).map(() => 0)
+    )
+  );
+
+
   // face 1 is the front face and face1 has size * size elements with coordination (0,0,0) to (0,size-1,size-1)
   let face1 = Array(size*size).fill(null).map(() =>  
     Array(size).fill(null).map(() => [0,0,0])
   );
+   
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       face1[i][j] = [0, i, j];
     }
   }
 
-  // face 2 is the back face and face2 has size * size elements with coordination (size-1,0,0) to (size-1,size-1,size-1)
-  let face2 = Array(size*size).fill(null).map(() =>  
-    Array(size).fill(null).map(() => [0,0,0])
-  );
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      face2[i][j] = [size-1, i, j];
+
+      let xcoor = Math.round(face1[i][j][0]);
+      let ycoor = Math.round(face1[i][j][1]);
+      let zcoor = Math.round(face1[i][j][2]);
+
+      //put coordination in the canvas
+      if (canvas[xcoor][ycoor][0] === null) {
+        canvas[xcoor][ycoor][0] = zcoor;
+      }
     }
   }
+
+  // // face 2 is the back face and face2 has size * size elements with coordination (size-1,0,0) to (size-1,size-1,size-1)
+  // let face2 = Array(size*size).fill(null).map(() =>  
+  //   Array(size).fill(null).map(() => [0,0,0])
+  // );
+  // for (let i = 0; i < size; i++) {
+  //   for (let j = 0; j < size; j++) {
+  //     face2[i][j] = [size-1, i, j];
+  //   }
+  // }
   
-  // face 3 is the left face and face3 has size * size elements with coordination (0,0,0) to (size-1,0,size-1)
-  let face3 = Array(size*size).fill(null).map(() =>  
-    Array(size).fill(null).map(() => [0,0,0])
-  );
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      face3[i][j] = [i, 0, j];
-    }
-  }
+  // // face 3 is the left face and face3 has size * size elements with coordination (0,0,0) to (size-1,0,size-1)
+  // let face3 = Array(size*size).fill(null).map(() =>  
+  //   Array(size).fill(null).map(() => [0,0,0])
+  // );
+  // for (let i = 0; i < size; i++) {
+  //   for (let j = 0; j < size; j++) {
+  //     face3[i][j] = [i, 0, j];
+  //   }
+  // }
 
-  // face 4 is the right face and face4 has size * size elements with coordination (0,size-1,0) to (size-1,size-1,size-1)
-  let face4 = Array(size*size).fill(null).map(() =>  
-    Array(size).fill(null).map(() => [0,0,0])
-  );
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      face4[i][j] = [i, size-1, j];
-    }
-  }
+  // // face 4 is the right face and face4 has size * size elements with coordination (0,size-1,0) to (size-1,size-1,size-1)
+  // let face4 = Array(size*size).fill(null).map(() =>  
+  //   Array(size).fill(null).map(() => [0,0,0])
+  // );
+  // for (let i = 0; i < size; i++) {
+  //   for (let j = 0; j < size; j++) {
+  //     face4[i][j] = [i, size-1, j];
+  //   }
+  // }
 
-  // face 5 is the top face and face5 has size * size elements with coordination (0,0,0) to (size-1,size-1,0)
-  let face5 = Array(size*size).fill(null).map(() =>  
-    Array(size).fill(null).map(() => [0,0,0])
-  );
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      face5[i][j] = [i, j, 0];
-    }
-  }
+  // // face 5 is the top face and face5 has size * size elements with coordination (0,0,0) to (size-1,size-1,0)
+  // let face5 = Array(size*size).fill(null).map(() =>  
+  //   Array(size).fill(null).map(() => [0,0,0])
+  // );
+  // for (let i = 0; i < size; i++) {
+  //   for (let j = 0; j < size; j++) {
+  //     face5[i][j] = [i, j, 0];
+  //   }
+  // }
 
-  // face 6 is the bottom face and face6 has size * size elements with coordination (0,0,size-1) to (size-1,size-1,size-1)
-  let face6 = Array(size*size).fill(null).map(() =>  
-    Array(size).fill(null).map(() => [0,0,0])
-  );
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      face6[i][j] = [i, j, size-1];
-    }
-  }
+  // // face 6 is the bottom face and face6 has size * size elements with coordination (0,0,size-1) to (size-1,size-1,size-1)
+  // let face6 = Array(size*size).fill(null).map(() =>  
+  //   Array(size).fill(null).map(() => [0,0,0])
+  // );
+  // for (let i = 0; i < size; i++) {
+  //   for (let j = 0; j < size; j++) {
+  //     face6[i][j] = [i, j, size-1];
+  //   }
+  // }
 
-
+  return canvas
 }
 
 //
-function render3DMatrix(matrix: number[][][]): string {
-  const size = matrix.length;
+function renderCanvas(size: number): string {
+  let canvas = calculateCube(size);
   let result = '';
 
-  for (let z = 0; z < size; z++) {
+  for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
-      for (let x = 0; x < size; x++) {
-        result += matrix[z][y][x] + ' ';
-      }
-      result += '\n';
+      result += canvas[x][y][0] + ' ';     
     }
     result += '\n';
   }
@@ -89,19 +104,9 @@ function render3DMatrix(matrix: number[][][]): string {
   return result;
 }
 
-
-function generateSquare(size: number): string {
-  let square = "";
-  const line = "@".repeat(size * 2) + "\n";
-  for (let i = 0; i < size; i++) {
-    square += line;
-  }
-  return square;
-}
-
-function Square() {
-  const size = 20; // Adjust the size as needed
-  const squareArt = generateSquare(size);
+function Canvas() {
+  const size = 10; // Adjust the size as needed
+  const squareArt = renderCanvas(size);
 
   return (
     <div style={{ lineHeight: '1.2em' }}>
@@ -118,7 +123,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
-        <Square/>
+        <Canvas/>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
